68814a6e81f36705a823f2b892611054caa1c03d
aguss <agussala2003@gmail.com>
2026-06-13 15:01:09 -0300

    Initial commit: Estructura base de paquetes

---
+++ src/modelo/Mago.java
@@ -0,0 +1,5 @@
+package modelo;
+
+public class Mago extends Personaje {
+
+}
---
+++ src/modelo/Personaje.java
@@ -0,0 +1,6 @@
+package modelo;
+
+public abstract class Personaje {
+	private String nombre;
+	private int nivelDeMagia;
+}
